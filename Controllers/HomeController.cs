62196876c47c417cfadc6d516b4b6cc18d530db7
Kyrios <Kyrios@DESKTOP-FLOOL9G>
2026-09-09 21:30:31 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EcommerceApp.Models;
+
+namespace EcommerceApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => RedirectToAction("Index", "Products");
+
+        public IActionResult Privacy() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
